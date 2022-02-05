b9de6b930436515d4c8ec2838648f523dd944fd7
limjaein <dlawodls96@naver.com>
2022-02-05 21:52:20 +0900

    #7 - 로그인 후 홈화면 이동 및 로그아웃 기능 추가

--- src/main/java/com/lgt/config/SecurityConfig.java
+++ src/main/java/com/lgt/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.lgt.service.OAuthService;
 
@@ -18,8 +20,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.oauth2Login() // OAuth2 로그인 설정 시작
-			.userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정을 저장 
-			.userService(oAuthService); // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록 
+		http.authorizeRequests()
+				.anyRequest()
+				.permitAll()
+			.and()
+				.csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and()
+				.logout()
+				.logoutSuccessUrl("/")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.and()
+				.oauth2Login() // OAuth2 로그인 설정 시작
+				.defaultSuccessUrl("/home")
+				.userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정을 저장 
+				.userService(oAuthService); // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록 
 	}
 }
