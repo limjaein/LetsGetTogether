aa45d0be866b9dee5486224da75c6254fb40a989
ky8778 <gunyoung8778@gamil.com>
2022-03-26 20:47:55 +0900

    [#9] 소스정리 - 사용 안하는 메서드 제거

--- src/main/java/com/lgt/controller/IndexController.java
+++ src/main/java/com/lgt/controller/IndexController.java
@@ -1,10 +1,5 @@
 package com.lgt.controller;
 
-import java.util.Optional;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -26,8 +21,4 @@ public class IndexController {
 		return "home";
 	}
 
-	// TODO: util 공통 함수로 빼기
-	private Optional<String> getAccessToken(HttpSession session) {
-		return Optional.ofNullable((String)session.getAttribute("access_token"));
-	}
 }
