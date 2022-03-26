509aa94fd4180a2665801bc12d79bda61f13e36c
ky8778 <gunyoung8778@gamil.com>
2022-03-26 20:34:29 +0900

    [#9] Kakao Friends 가져오기 API 추가 - responseDto, Friend 클래스 추가 - KakaoAPI Service 로 이동   (spring container 에서 관리할 수 있도록) - Controller, Service 로직 추가

--- src/main/java/com/lgt/controller/UserController.java
+++ src/main/java/com/lgt/controller/UserController.java
@@ -1,34 +1,36 @@
 package com.lgt.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.lgt.kakao.KakaoAPI;
+import com.lgt.domain.Friend;
+import com.lgt.service.KakaoAPIService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
-	private KakaoAPI kakaoapi;
-
-	public UserController() {
-		kakaoapi = new KakaoAPI();
-	}
+	private final KakaoAPIService kakaoAPIService;
 
 	@RequestMapping("/user/logout")
 	public String logout(HttpServletRequest request) {
 		HttpSession session = request.getSession();
-		Optional<String> accessToken = getAccessToken(request.getSession());
+		Optional<String> accessToken = getAccessToken(session);
 
 		if (accessToken.isPresent() && !"".equals(accessToken.get())) {
-			kakaoapi.logout(accessToken.get());
+			kakaoAPIService.logout(accessToken.get());
 			session.removeAttribute("access_token");
 			session.removeAttribute("user");
 			log.info("logout success");
@@ -37,6 +39,22 @@ public class UserController {
 		return "redirect:/";
 	}
 
+	@GetMapping("/user/friends")
+	public String friends(HttpServletRequest request, Model model) {
+		Optional<String> accessToken = getAccessToken(request.getSession());
+
+		if (accessToken.isPresent() && !"".equals(accessToken.get())) {
+			Optional<List<Friend>> freinds = kakaoAPIService.getFreinds(accessToken.get());
+
+			if (freinds.isPresent()) {
+				model.addAttribute("friends", freinds.get());
+			}
+		}
+
+		return "friends_list";
+	}
+
+	// TODO: util 공통 함수로 빼기
 	private Optional<String> getAccessToken(HttpSession session) {
 		return Optional.ofNullable((String)session.getAttribute("access_token"));
 	}
