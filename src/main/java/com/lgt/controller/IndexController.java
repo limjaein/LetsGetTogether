9eb4707ece2936ce95aee1fedf55a4facc82ca24
ky8778 <gunyoung8778@gmail.com>
2022-04-02 12:46:18 +0900

    [#3] 테스트용 친구리스트 fragment 구현 - Kakao Friends API request 결과 보여주는 테스트용 fragment 작업

--- src/main/java/com/lgt/controller/IndexController.java
+++ src/main/java/com/lgt/controller/IndexController.java
@@ -1,8 +1,16 @@
 package com.lgt.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.lgt.domain.Friend;
+import com.lgt.service.KakaoAPIService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,14 +19,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class IndexController {
 
+	private final KakaoAPIService kakaoAPIService;
+
 	@GetMapping("/")
 	public String index() {
-		return "index";
+		return "webapp/bootstrap-index";
 	}
 
 	@GetMapping("/home")
-	public String home() {
-		return "home";
+	public String home(HttpSession session, Model model) {
+		Optional<String> accessToken = Optional.ofNullable((String)session.getAttribute("access_token"));
+
+		if (accessToken.isPresent() && !"".equals(accessToken.get())) {
+			Optional<List<Friend>> freinds = kakaoAPIService.getFreinds(accessToken.get());
+			if (freinds.isPresent()) {
+				model.addAttribute("friends", freinds.get());
+			}
+		}
+		return "webapp/bootstrap-home";
 	}
 
 }
